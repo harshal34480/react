85d51e0ea28296e68ca1b7310df8ae2c606eca59
harshal34480 <harshal34480@gmail.com>
2024-01-14 17:07:26 +0530

    useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,26 @@
 import RestCard from "./RestCard";
 import { resObj } from "../utils/raw_data";
+import { useState } from "react";
 const Body = () => {
+  let [listOfResturant, updateResturant] = useState(resObj);
+
   return (
     <div className="body">
       <div className="filter">
         <button
           onClick={() => {
-            resObj = resObj.filter((res) => res.info.avgRating > 4);
+            const newList = listOfResturant.filter(
+              (res) => res.info.avgRating < 4
+            );
+            console.log(newList);
+            updateResturant(newList);
           }}>
           Click me
         </button>
       </div>
 
       <div className="restContainer">
-        {resObj.map((resturant) => (
+        {listOfResturant.map((resturant) => (
           <RestCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
