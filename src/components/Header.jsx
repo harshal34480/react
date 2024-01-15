69e4fc85efa7fe46b9243263aad66a45c48984fe
Harshal Wankhade <harshal34480@gmail.com>
2024-01-15 23:10:02 +0530

    add login/logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import { APP_LOGO } from "../utils/essentials";
+import { useState } from "react";
+
 const Header = () => {
+  const [btn, setBtn] = useState("Login");
+
   return (
     <div className="header">
       <div className="headerLogo">
@@ -11,6 +15,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button
+              onClick={() => {
+                btn == "Login" ? setBtn("Logout") : setBtn("Login");
+              }}>
+              {btn}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
