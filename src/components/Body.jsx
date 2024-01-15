2aa640a1d479f01ab8c5f2bf96585744d8c4d158
Harshal Wankhade <harshal34480@gmail.com>
2024-01-15 23:09:26 +0530

    add api and useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,27 @@
 import RestCard from "./RestCard";
-import { resObj } from "../utils/raw_data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  let [listOfResturant, updateResturant] = useState(resObj);
+  let [listOfResturant, updateResturant] = useState([]);
 
-  return (
+  useEffect(() => {
+    apiData();
+  }, []);
+
+  const apiData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.9319821&lng=77.7523039&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    updateResturant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfResturant == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
