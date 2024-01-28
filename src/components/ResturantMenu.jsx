eb9c6e220417adb92f0a836ed8ca02bc48245abe
Harshal Wankhade <harshal34480@gmail.com>
2024-01-28 23:33:39 +0530

    modify resturentMenu component

--- src/components/ResturantMenu.jsx
+++ src/components/ResturantMenu.jsx
@@ -1,19 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const ResturantMenu = () => {
+  const [resInfo, setresInfo] = useState();
+  console.log("body");
   useEffect(() => {
     fetchResturant();
   }, []);
 
   const fetchResturant = async () => {
     const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.9319821&lng=77.7523039&restaurantId=300767&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
+    setresInfo(json);
   };
+
+  const { name, costForTwoMessage } = resInfo?.data?.cards[0]?.card?.card?.info;
+  const { itemCard } =
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card;
+  console.log(itemCard);
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
   return (
     <div className="resturant-menu">
-      <h1>Resturant Name</h1>
+      <h1>{name}</h1>
+      <h2>{costForTwoMessage}</h2>
       <ul>
         <li>Maggie</li>
         <li>Samosa</li>
