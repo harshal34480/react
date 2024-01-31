2597ab24fced1ae5305957352d4a242cd19d0f38
Harshal Wankhade <harshal34480@gmail.com>
2024-01-31 23:02:26 +0530

    add userClass component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,35 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>Created by Harshal</h2>
-      <p>food delivery app</p>
-      <User
-        name={"Harsahl Wankhade(Functional Component)"}
-        location={"Amravati"}
-      />
-      <UserClass
-        name={"Harshal Wankhade (class component)"}
-        location={"Nagpur"}
-      />
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor() {
+    super();
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent mount");
+  }
+
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>Created by Harshal</h2>
+        <p>food delivery app</p>
+
+        <UserClass
+          name={"Harshal Wankhade (class component)"}
+          location={"Nagpur"}
+        />
+        {/* <UserClass
+          name={"Harshal Wankhade (class component)"}
+          location={"Nagpur"}
+        /> */}
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,13 +2,33 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count: 0,
+      count2: 3,
+    };
+
+    console.log("children constructor");
+  }
+
+  componentDidMount() {
+    console.log("child mount");
   }
 
   render() {
-    const { name, location } = props.this;
+    console.log("children render");
+    const { name, location } = this.props;
     return (
       <div className="user-card">
+        <h1>Count = {this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}>
+          Count changer
+        </button>
         <h1>User : {name}</h1>
         <h3>Location : {location}</h3>
         <h3>Contact : harshal@34480</h3>
